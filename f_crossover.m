b96119e83f4e33e1db3ee476f1396358a549dc8e
deasymaharani <deasylver@gmail.com>
2016-05-27 21:11:24 +1000

    edited

--- f_crossover.m
+++ f_crossover.m
@@ -1,28 +1,9 @@
 function [ newPop5 ] = f_crossover( newPop5,nPop,alpha )
-%PXover=0.8;
+
 first=0;
 [x,y] = size(newPop5(1).chromosome);
-%alpha=0.9;
-
-% for i=1:nPop
-%     px =rand();
-%     if px < PXover
-%         first=first+1;
-%         if mod(first,2)==0
-%             for j=1:point-1
-%                 temp = newPop5(one).chromosome(j);
-%                 newPop5(one).chromosome(j) = newPop5(i).chromosome(j);
-%                 newPop5(i).chromosome(j) = temp;
-%             end
-%         else
-%             one = i;
-%         end
-%     end
-% end
 
 for i=1:nPop
-    %px =rand();
-    %if px < PXover
         first=first+1;
         if mod(first,2)==0
             point=randi([1 x]);
@@ -34,7 +15,6 @@ for i=1:nPop
         else
             one = i;
         end
-    %end
 end
 
 end
--- f_mutation.m
+++ f_mutation.m
@@ -1,15 +1,10 @@
 function [ newPop5 ] = f_mutation( PMUT,newPop5,nPop,t,ub,lb,b,n_mut )
 [x,y] = size(newPop5(1).chromosome);
-%PMUT=0.08;
-%b=5;
-% t=2;
 T=100;
 rl = 0.0001; ru=0.1;
-% rl = 1; ru = 1000;
+
 for i=1:nPop
     p=(ru-rl).*rand()+rl;
-    %p=randi([rl ru]);
-    %if p/10000<PMUT
     if p<PMUT
         p=int64(p*10000);
         if mod(p,2)==0
@@ -33,16 +28,3 @@ for i=1:nPop
 end
  
 end
-
-% for i=1:nPop
-%     p=rand();
-%     if p<PMUT
-%         for k=1:2
-%         j(i,k) = randi([1 x]);
-%         %fprintf('mutation index at %d.\n',j);
-%         end
-%         temp = newPop5(i).chromosome(j(i,1));
-%         newPop5(i).chromosome(j(i,1)) = newPop5(i).chromosome(j(i,2));
-%         newPop5(i).chromosome(j(i,2)) = temp;
-%     end
-% end
